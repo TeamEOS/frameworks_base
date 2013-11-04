6f15374aebe68c767fc9a27cdc0815720c7e58d1
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-11-04 02:39:44 +0000

    Port "Option to control cursor in text fields using volume keys (1/2)"
    This feature is moved to framework so it also works with third party keyboards.
    Patch Set 3: Added missin @hide statements
    
    Change-Id: I8e20240e7bee5351ab20bb3d701eb95a5fd3e112
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2817,6 +2817,15 @@ public final class Settings {
          */
         public static final String KEY_APP_SWITCH_LONG_PRESS_ACTION = "key_app_switch_long_press_action";
         /**
+          * Volume keys control cursor in text fields (default is 0)
+          * 0 - Disabled
+          * 1 - Volume up/down moves cursor left/right
+          * 2 - Volume up/down moves cursor right/left
+          * @hide
+          */
+         public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
