40c6915e43be05ef176023b20303827769c11dbf
Jun Su <juns@nvidia.com>
2014-01-19 12:43:58 +0800

    SystemUI: disable statusbar time refresh when screen off
    When screen is off, don't refresh the status bar to save power.
    When screen is on, refresh immediately for user experience.
    
    Change-Id: Ie250e20bab57c096a16886680a53263c635bf6f4

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -96,6 +96,8 @@ public class Clock extends TextView implements DemoMode {
             filter.addAction(Intent.ACTION_TIMEZONE_CHANGED);
             filter.addAction(Intent.ACTION_CONFIGURATION_CHANGED);
             filter.addAction(Intent.ACTION_USER_SWITCHED);
+            filter.addAction(Intent.ACTION_SCREEN_ON);
+            filter.addAction(Intent.ACTION_SCREEN_OFF);
 
             getContext().registerReceiver(mIntentReceiver, filter, null, getHandler());
         }
@@ -141,6 +143,16 @@ public class Clock extends TextView implements DemoMode {
                 }
             }
             updateAmPm();
+
+            if (action.equals(Intent.ACTION_SCREEN_ON)) {
+                mScreenOn = true;
+            } else if (action.equals(Intent.ACTION_SCREEN_OFF)) {
+                mScreenOn = false;
+            }
+
+            if (mScreenOn) {
+                updateClock();
+            }
         }
     };
 
