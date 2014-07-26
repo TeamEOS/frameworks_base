a8c04d640bdedc2096c1d347cf97f82f49bedfaa
Danesh M <daneshm90@gmail.com>
2014-07-25 19:02:26 -0700

    Framework : Fix proximity wakeup reference
    Change-Id: Ia40ca6fbf6e37fbb3996f2384f725c30b88c2131

--- core/java/android/os/PowerManager.java
+++ core/java/android/os/PowerManager.java
@@ -515,7 +515,7 @@ public final class PowerManager {
      */
     public void wakeUpWithProximityCheck(long time) {
         try {
-            mService.wakeUpFromKeyEvent(time);
+            mService.wakeUpWithProximityCheck(time);
         } catch (RemoteException e) {
         }
     }
