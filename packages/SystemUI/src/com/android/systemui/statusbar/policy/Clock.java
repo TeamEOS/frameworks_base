TeamEOS/frameworks_base

commit 40c6915e43be05ef176023b20303827769c11dbf

SystemUI: disable statusbar time refresh when screen off
When screen is off, don't refresh the status bar to save power.
When screen is on, refresh immediately for user experience.

Change-Id: Ie250e20bab57c096a16886680a53263c635bf6f4

packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -96,6 +96,8 @@ public class Clock extends TextView implements DemoMode {
             filter.addAction(Intent.ACTION_TIMEZONE_CHANGED);
             filter.addAction(Intent.ACTION_CONFIGURATION_CHANGED);
             filter.addAction(Intent.ACTION_USER_SWITCHED);
+            filter.addAction(Intent.ACTION_SCREEN_ON);
+            filter.addAction(Intent.ACTION_SCREEN_OFF);
 
             getContext().registerReceiver(mIntentReceiver, filter, null, getHandler());
         }
@@ -141,6 +143,16 @@ public class Clock extends TextView implements DemoMode {
                 }
             }
             updateAmPm();
+
+            if (action.equals(Intent.ACTION_SCREEN_ON)) {
+                mScreenOn = true;
+            } else if (action.equals(Intent.ACTION_SCREEN_OFF)) {
+                mScreenOn = false;
+            }
+
+            if (mScreenOn) {
+                updateClock();
+            }
         }
     };
 

packages/SystemUI/src/com/android/systemui/statusbar/policy/DateView.java
@@ -41,11 +41,18 @@ public class DateView extends TextView {
 
     private SimpleDateFormat mDateFormat;
     private String mLastText;
+    private boolean mScreenOn = true;
 
     private BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             final String action = intent.getAction();
+            if (action.equals(Intent.ACTION_SCREEN_ON)) {
+                mScreenOn = true;
+            } else if (action.equals(Intent.ACTION_SCREEN_OFF)) {
+                mScreenOn = false;
+            }
+
             if (Intent.ACTION_TIME_TICK.equals(action)
                     || Intent.ACTION_TIME_CHANGED.equals(action)
                     || Intent.ACTION_TIMEZONE_CHANGED.equals(action)
@@ -55,7 +62,9 @@ public class DateView extends TextView {
                     // need to get a fresh date format
                     mDateFormat = null;
                 }
-                updateClock();
+                if (mScreenOn) {
+                    updateClock();
+                }
             }
         }
     };
@@ -69,6 +78,8 @@ public class DateView extends TextView {
         super.onAttachedToWindow();
 
         IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_SCREEN_ON);
+        filter.addAction(Intent.ACTION_SCREEN_OFF);
         filter.addAction(Intent.ACTION_TIME_TICK);
         filter.addAction(Intent.ACTION_TIME_CHANGED);
         filter.addAction(Intent.ACTION_TIMEZONE_CHANGED);

commit a83fda3cd3f54dec3155c76f566012a7877fc58b

Fix uninitialized mScreenOn boolean in Clock
Fixes fail introduced from

https://github.com/TeamEOS/frameworks_base/commit/40c6915e43be05ef176023b20303827769c11dbf

Change-Id: I9daf426c588451a0a642951fba7c2d04e65acb3a

packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -63,6 +63,7 @@ public class Clock extends TextView implements DemoMode {
     // set bools at init for easy management
     private boolean mIsSignalView = false;
     private boolean mIsCenterView = false;
+    private boolean mScreenOn = true;
 
     public Clock(Context context) {
         this(context, null);