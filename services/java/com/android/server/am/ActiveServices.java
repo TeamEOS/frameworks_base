20e97ca2715c8d82d167b0139f98f1f1626b4650
louis_chang <louis_chang@htc.com>
2014-08-25 17:20:55 +0800

    [ActivityManager] Fix ServiceRecord leakage
    Symptom:
    The first step of cleaning up application services is
    to clear the app state from services (which set sr.app
    to null). Then it clean up the service connections.
    In some scenario, the Service might be removed during
    the connection cleanup (i.e. the Service is no longer
    needed). In that case, the Service will be removed from
    ServiceMap, but won't be removed from r.app.services
    (line 1612) because the r.app is null.
    
    Solution:
    Remove the service connection first.
    
    Change-Id: I644d73af58fe0e7c1c4a6c9779fe6b5d747b880d

--- services/java/com/android/server/am/ActiveServices.java
+++ services/java/com/android/server/am/ActiveServices.java
@@ -2038,7 +2038,14 @@ public final class ActiveServices {
             }
         }
 
-        // First clear app state from services.
+        // Clean up any connections this application has to other services.
+        for (int i=app.connections.size()-1; i>=0; i--) {
+            ConnectionRecord r = app.connections.valueAt(i);
+            removeConnectionLocked(r, app, null);
+        }
+        app.connections.clear();
+
+        // Clear app state from services.
         for (int i=app.services.size()-1; i>=0; i--) {
             ServiceRecord sr = app.services.valueAt(i);
             synchronized (sr.stats.getBatteryStats()) {
@@ -2064,13 +2071,6 @@ public final class ActiveServices {
                 b.requested = b.received = b.hasBound = false;
             }
         }
-
-        // Clean up any connections this application has to other services.
-        for (int i=app.connections.size()-1; i>=0; i--) {
-            ConnectionRecord r = app.connections.valueAt(i);
-            removeConnectionLocked(r, app, null);
-        }
-        app.connections.clear();
 
         ServiceMap smap = getServiceMap(app.userId);
 
