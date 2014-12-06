3879a4805028996519d086d0d07c732f27547a0b
Byunghun Jeon <bjeon@codeaurora.org>
2014-12-05 18:44:11 -0800

    WindowManager: Upper level changes to expose blur effect
    Upper level changes to expose blur-behind and blur-mask effect
    
    Change-Id: I6d37b43888c8c5e028974bd714596d8178cb5114
    
    WindowManager: Adding template for BlurLayer file
    
    Cyanogen changes:
      * Use config.xml instead of settings
      * Don't break stuff if not supported in SF
      * Incremental fade for insecure lockscreen
      * Disabled extra effects for now
    
    Change-Id: Icefe452f7a015656661e8543849c9b88889dbb40

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2499,6 +2499,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         return mScreenOnComingFromTouch;
     }
 
+    void setBlur(float b){
+        mStatusBarWindowManager.setBlur(b);
+    }
+
     public boolean isFalsingThresholdNeeded() {
         boolean onKeyguard = getBarState() == StatusBarState.KEYGUARD;
         boolean isMethodInsecure = mUnlockMethodCache.isMethodInsecure();
@@ -3470,7 +3474,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private void addStatusBarWindow() {
         makeStatusBarView();
         mStatusBarWindow.addContent(mStatusBarWindowContent);
-        mStatusBarWindowManager = new StatusBarWindowManager(mContext);
+        mStatusBarWindowManager = new StatusBarWindowManager(mContext, this);
         mStatusBarWindowManager.add(mStatusBarWindow, getStatusBarHeight());
     }
 
@@ -4242,6 +4246,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
         updateKeyguardState(staying, false /* fromShadeLocked */);
         return staying;
+    }
+
+    boolean isSecure() {
+        return mStatusBarKeyguardViewManager != null && mStatusBarKeyguardViewManager.isSecure();
     }
 
     public long calculateGoingToFullShadeDelay() {
