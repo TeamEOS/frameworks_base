489a0df82772cec89b684f5d7a9fce21f9df6e9e
Randall Rushing <randall.rushing@gmail.com>
2015-01-19 19:04:55 -0800

    Post theme engine bring-up clean up
    -- https://github.com/CyanogenMod/android_frameworks_base/commit/f2098426f0347a034eaf0d25903d37f5817a211b#diff-861aa855219bd25761dc6f44dfb97937R3651
    
    -- NxBarTransitions- update constructor
    
    Change-Id: I827b6fb9a857ce121769d8f187c4c73541fcdd6d

--- packages/SystemUI/src/com/android/systemui/nx/NxBarTransitions.java
+++ packages/SystemUI/src/com/android/systemui/nx/NxBarTransitions.java
@@ -39,7 +39,10 @@ public final class NxBarTransitions extends BarTransitions {
     private int mRequestedMode;
 
     public NxBarTransitions(NxBarView view) {
-        super(view, R.drawable.nav_background);
+        super(view, R.drawable.nav_background, R.color.navigation_bar_background_opaque,
+                R.color.navigation_bar_background_semi_transparent,
+                R.color.navigation_bar_background_transparent,
+                com.android.internal.R.color.battery_saver_mode_color);
         mView = view;
         mBarService = IStatusBarService.Stub.asInterface(
                 ServiceManager.getService(Context.STATUS_BAR_SERVICE));
