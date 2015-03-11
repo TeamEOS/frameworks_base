38c11465bfd13a89368b8c5ede1c1a725ab71a12
XXMrHyde <xxmrhyde@gmx.co.uk>
2015-03-11 14:04:02 +0100

    Lock screen: Weather panel improvements, (1/2):
    - Move weather panel to the bottom the clock panel
    - Add wind
    - Add humidity
    - Add Condition
    - Add Timestamp
    - Add condition icons (colored and VClouds)
    - Rearrange the weather panel:
      - Left side:
        - Location
        - Wind
      - Center:
        - Condiditon icon
      - Right side:
        - Current temperature
        - Humidity
      - Bottom:
        - Condition
        - Timestamp
    - Option to show/hide the timestamp
    - Option to colorize the monochrome icons
    
    Change-Id: I986975c13c5d29bd8103424e020ad775a8f03804
    
    This also includes:
    FWB: Proper LS weather switch from XXMrHyde
    https://github.com/AICP/frameworks_base/commit/ff1ceb40e268dda12906b4bfffd78df849430de1
    
    Weather: Handle no weather information available, (1/2)
    https://github.com/AICP/frameworks_base/commit/b1406dc8561ff1043bf357dcef9962e18a33ad63
    
    Lock screen: Update Handle no weather information available
    https://github.com/AICP/frameworks_base/commit/3691a5875edddc824e687a905ac413b3b2677160
    
    Change-Id: I72b477db49e7a6b3f2f358e8f9aa23bca542a054

--- core/java/com/android/internal/logging/MetricsConstants.java
+++ core/java/com/android/internal/logging/MetricsConstants.java
@@ -264,6 +264,9 @@ public interface MetricsConstants {
     public static final int TUNER_BATTERY_PERCENTAGE = 237;
     public static final int FUELGAUGE_INACTIVE_APPS = 238;
 
+    // Lock screen weather settings
+    public static final int LOCKSCREEN_WEATHER_SETTINGS = 239;
+
     // These constants must match those in the analytic pipeline, do not edit.
     // Add temporary values to the top of MetricsLogger instead.
 
