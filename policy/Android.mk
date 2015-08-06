af0c242246104228c3ed217c97f62c473eb520fa
Randall Rushing <randall.rushing@gmail.com>
2015-08-05 23:26:17 -0700

    Navigation: shared library is not needed at this time
    We will require a shared library when IPC/Binder/Service is needed
    
    Change-Id: I8689cfee214efc5c33ac7fb6f167fdfbc5d73ee4

--- policy/Android.mk
+++ policy/Android.mk
@@ -5,8 +5,6 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
-LOCAL_JAVA_LIBRARIES := org.teameos.navigation
-LOCAL_REQUIRED_MODULES := org.teameos.navigation
 LOCAL_MODULE := android.policy
 LOCAL_STATIC_JAVA_LIBRARIES := \
     org.cyanogenmod.platform.sdk \
