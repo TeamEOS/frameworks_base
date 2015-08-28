3d0db9d9094de1314b74c055e437248df2abfc98
LorDClockaN <davor@losinj.com>
2015-08-28 20:59:06 +0200

    Toast icon switch [1/2]
    - Turn off by default
    
    Change-Id: I59ffd10f20a2ae9491f76380ec9af641e0844928

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3948,6 +3948,13 @@ public final class Settings {
         public static final String T9_SEARCH_INPUT_LOCALE = "t9_search_input_locale";
 
         /**
+         * Toast icon
+         *
+         * @hide
+         */
+        public static final String TOAST_ICON = "toast_icon";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
