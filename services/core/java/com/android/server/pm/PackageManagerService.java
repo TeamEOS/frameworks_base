bbc22e24cd5f876ac20e01bf6e21616c92001936
d34d <clark@cyngn.com>
2015-10-28 10:49:41 -0700

    Themes: Stop using ThemeUtils.getBootThemeDirty()
    Settings are no longer stored in a SQLite DB so this method is no
    longer applicable
    
    Change-Id: If46d5d854fe8b4c97f91fdfb4f663f2256d5fbf6

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -2129,7 +2129,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                 }
             }
 
-            mBootThemeConfig = ThemeUtils.getBootThemeDirty();
+            mBootThemeConfig = ThemeConfig.getSystemTheme();
 
             // Collect vendor overlay packages.
             // (Do this before scanning any apps.)
